b813a89a03bfc97f68dccaacceda2b65bc7c379c
Alexander Morley <alex@alexmorley.me>
2020-08-20 07:28:48 +0100

    download int16 with metadata

--- scripts/download_all.py
+++ scripts/download_all.py
@@ -4,6 +4,7 @@ import json
 from pathlib import Path
 
 from slugify import slugify
+import numpy as np
 
 sys.path.append('.')
 import neuropixels_data_sep_2020 as nd
@@ -12,13 +13,33 @@ import spikeextractors as se
 with open('./known_recordings.json') as f:
     known_recordings = json.load(f)
 
-recording_ids = [r['recordingId'] for r in known_recordings['recordings']]
+recording_ids = [r['recordingId'] for r in known_recordings['recordings'] if r['recordingId'].endswith('10sec')]
 
 output_dir = Path(os.environ.get('RECORDINGS_OUTPUT_DIR', 'recordings/'))
 os.makedirs(output_dir, exist_ok=True)
 for i, recording_id in enumerate(recording_ids):
-    filename = f'{slugify(recording_id)}.dat'
+    r_id = slugify(recording_id)
+    filename = f'{r_id}.dat'
     print(f'Creating ({i+1} of {len(recording_ids)}): {output_dir / filename}')
     recording = nd.load_recording(recording_id)
-    se.BinDatRecordingExtractor.write_recording(recording, output_dir / filename)
+    np.savetxt(
+            output_dir / f"{r_id}.channel_locations.csv",
+            recording.get_channel_locations(),
+            delimiter=",", fmt="%d"
+    )
+    np.savetxt(
+            output_dir / f"{r_id}.channel_ids.csv",
+            recording.get_channel_ids(),
+            delimiter=",", fmt="%d"
+    )
+    np.savetxt(
+            output_dir / f"{r_id}.num_channels.csv",
+            [recording.get_num_channels()],
+            delimiter=",", fmt="%d"
+    )
+    se.BinDatRecordingExtractor.write_recording(
+        recording,
+        output_dir / filename,
+        dtype='int16'
+    )
     print(f'########### DONE {output_dir / filename} ###########')
