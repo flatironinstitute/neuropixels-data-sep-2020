0fff1803d12fdbd4127b7dd7dd7eb75e8c94166e
Jeremy Magland <jmagland@flatironinstitute.org>
2020-08-24 14:09:17 -0400

    rearrange known recordings

--- neuropixels_data_sep_2020/recordings.py
+++ neuropixels_data_sep_2020/recordings.py
@@ -4,50 +4,30 @@ import json
 import os
 from pathlib import Path
 from typing import List, Any
+import kachery_p2p as kp
 
 from .extractors import LabboxEphysRecordingExtractor, LabboxEphysSortingExtractor
+from .known_recordings_uri import KNOWN_RECORDINGS_URI
 
 module_name = 'neuropixels-data-sep-2020'
 
-def get_recordings_file_path(*, readonly: bool = False) -> str:
-    module_name = 'neuropixels-data-sep-2020'
-    p = Path(os.getcwd())
+def load_known_recordings_dict():
+    try:
+        x = kp.load_object(KNOWN_RECORDINGS_URI)
+    except:
+        raise Exception('Problem loading recordings dict. Perhaps you are not running the kachery-p2p daemon?')
+    return x
 
-    if (not module_name in str(p)):
-        raise Exception(f"""
-    Recordings-file could not be located.
-    Please ensure that you run from within the {module_name} directory.
-    """)
-    basepath = p
-    for x in p.parents:
-        if module_name in str(x):
-            basepath = x
+def get_valid_recordings() -> List[Any]:
+    x = load_known_recordings_dict()
+    return x['recordings']
 
-    known_recordings_file = f"{basepath}/known_recordings.json"
-    file_exists = Path(known_recordings_file).exists()
-    namestr = f"Known-recordings file {known_recordings_file}"
-    if (not file_exists and readonly):
-        raise Exception(f"{namestr} does not exist and can't be opened for reading.")
-    if (file_exists and not readonly):
-        raise Exception(f"""
-    {namestr} already exists; aborting.
-    If you intended to overwrite it, please delete it first.""")
-
-    return known_recordings_file
-
-
-def get_valid_recordings(fname: str) -> List[Any]:
-    with open(fname, 'r') as fp:
-        recordings: List[Any] = json.load(fp)['recordings']
-        return recordings
-
-def get_valid_sortings(fname: str) -> List[Any]:
-    with open(fname, 'r') as fp:
-        recordings: List[Any] = json.load(fp)['sortings']
-        return recordings
+def get_valid_sortings() -> List[Any]:
+    x = load_known_recordings_dict()
+    return x['sortings']
 
 def load_recording(rec_id: str) -> Any:
-    valid_recordings: List[Any] = get_valid_recordings(get_recordings_file_path(readonly=True))
+    valid_recordings: List[Any] = get_valid_recordings()
     for entry in valid_recordings:
         rec_json = f"{rec_id}.json"
         if (rec_id == entry['recordingId'] or rec_id == entry['recordingLabel']
@@ -58,7 +38,7 @@ def load_recording(rec_id: str) -> Any:
     raise Exception(f"Requested recording with identifier '{rec_id}' is not recognized.")
 
 def load_sorting(sorting_id: str) -> Any:
-    valid_sortings: List[Any] = get_valid_sortings(get_recordings_file_path(readonly=True))
+    valid_sortings: List[Any] = get_valid_sortings()
     for entry in valid_sortings:
         sorting_json = f"{sorting_id}.json"
         if (sorting_id == entry['sortingId'] or sorting_id == entry['sortingLabel']
